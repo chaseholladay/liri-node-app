046d610956b1d40a63178c331998f744a888fa08
Chase Holladay <chaseholladay@gmail.com>
2018-11-14 12:16:38 -0600

    debugin file paths

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var keys = require('/keys');
+var keys = require('./keys');
 var Spotify = require('node-spotify-api');
 //added to format table 
 var cTable = require('console.table');
